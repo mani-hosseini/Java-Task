14d6bfaa3e880a3f33ee3f6f5f4ea24010d3bacb
Mani hosseini <mani.hosseini.h8@gmail.com>
2025-02-04 12:44:28 +0330

    task 2 of javascript complete

--- app.js
+++ app.js
@@ -12,3 +12,11 @@ function filterArray(myArray) {
 const arrayNumber = [2, 3, 4, 5, 22, 5, 7, 80];
 console.log(filterArray(arrayNumber));
 
+
+// Task 2
+function cleanArray(mainArray) {
+    return mainArray.flat(Infinity)
+}
+
+const array = [1, 2, [3, 4], [5, [6]], 7]
+console.log(cleanArray(array));
