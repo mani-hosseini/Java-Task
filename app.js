db9279dbd157dc71e1d67c96963bfd5c785afb8e
Mani hosseini <mani.hosseini.h8@gmail.com>
2025-02-04 14:33:30 +0330

    task 3 of javascript complete

--- app.js
+++ app.js
@@ -1,22 +1,43 @@
-// Task 1
-function filterArray(myArray) {
-    const evenNumber = [];
-    for (let i = 0; i < myArray.length; i++) {
-        if (myArray[i] % 2 === 0) {
-            evenNumber.push(myArray[i]);
-        }
+// // Task 1
+// function filterArray(myArray) {
+//     const evenNumber = [];
+//     for (let i = 0; i < myArray.length; i++) {
+//         if (myArray[i] % 2 === 0) {
+//             evenNumber.push(myArray[i]);
+//         }
+//     }
+//     return evenNumber;
+// }
+//
+// const arrayNumber = [2, 3, 4, 5, 22, 5, 7, 80];
+// console.log(filterArray(arrayNumber));
+//
+//
+// // Task 2
+// function cleanArray(mainArray) {
+//     return mainArray.flat(Infinity)
+// }
+//
+// const array = [1, 2, [3, 4], [5, [6]], 7]
+// console.log(cleanArray(array));
+
+
+// Task 3
+async function getDate() {
+    const span = document.getElementById('span');
+    try {
+        span.innerText = "loading....";
+        const url = 'https://api.chucknorris.io/jokes/random';
+        const api = await fetch(url, {
+            method: 'GET',
+        });
+
+        const data = await api.json();
+        span.innerText = data.value;
+
+    } catch (error) {
+        console.error(error);
     }
-    return evenNumber;
 }
 
-const arrayNumber = [2, 3, 4, 5, 22, 5, 7, 80];
-console.log(filterArray(arrayNumber));
-
-
-// Task 2
-function cleanArray(mainArray) {
-    return mainArray.flat(Infinity)
-}
-
-const array = [1, 2, [3, 4], [5, [6]], 7]
-console.log(cleanArray(array));
+document.getElementById('btn').addEventListener('click', getDate);
